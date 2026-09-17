273f51c41cc190531509e54e3a1509b6f28900be
Tanabordi <tanabm115@gmail.com>
2026-09-17 22:09:55 +0700

    rush

--- cell04/rush/script.js
+++ cell04/rush/script.js
@@ -1,13 +1,2 @@
 $(document).ready(function() {
-    $('a.nav-link').on('click', function(event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            var hash = this.hash;
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top - 60
-            }, 800, function(){
-                window.location.hash = hash;
-            });
-        }
-    });
 });
